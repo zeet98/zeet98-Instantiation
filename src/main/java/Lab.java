29c24fe8e8cdf914217c581e8448ed5c959e173e
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-22 01:50:20 +0000

    Completed instantiation assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object bread = new Object();
+        return bread;
     }
 }
